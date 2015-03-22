65ac19d8c90ced37e8f6601c847ba5d59ae426b8
Miguel Mota <miguelmota2@gmail.com>
2015-03-21 23:56:08 -0700

    Improve isElement checks by checking the objects stringified type rather than using instanceof because instanceof throws an exception if the HTMLElement constructor does not exist.
    Modify exports to include AMD support and browser export.
    
    Add bower support.

--- is-element.js
+++ is-element.js
@@ -1,6 +1,21 @@
-module.exports = function(value){
-  return value !== undefined
-    && typeof HTMLElement !== 'undefined'
-    && value instanceof HTMLElement
-    && value.nodeType === 1;
-}
+(function(root) {
+  function isElement(value) {
+    return (value && value.nodeType === 1) &&
+           (value && typeof value == 'object') &&
+           (Object.prototype.toString.call(value).indexOf('Element') > -1);
+  }
+
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = isElement;
+    }
+    exports.isElement = isElement;
+  } else if (typeof define === 'function' && define.amd) {
+    define([], function() {
+      return isElement;
+    });
+  } else {
+    root.isElement = isElement;
+  }
+
+})(this);
